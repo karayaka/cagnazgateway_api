b64869773627c5e17c37376fe5f74571f3379e1b
cagrikarayaka <cagri.karayaka@gmail.com>
2025-04-22 19:48:59 +0300

    first commit

---
+++ Cagnaz.Gateway/Program.cs
@@ -0,0 +1,44 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Configuration.AddJsonFile("ocelot.json",optional:false,reloadOnChange:true);
+builder.Services.AddOcelot(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/ping", () =>
+{
+    return DateTime.Now;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+await app.UseOcelot();
+
+app.Run();
+
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
